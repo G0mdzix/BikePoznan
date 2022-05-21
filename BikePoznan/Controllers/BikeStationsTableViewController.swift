90d5d6266ffe5c06904f1d258a1a28cf4b61b0d0
G0mdzix <mateusz@gozdzik.xyz>
2022-05-21 12:41:40 +0200

    Displaying data

--- BikePoznan/Controllers/BikeStationsTableViewController.swift
+++ BikePoznan/Controllers/BikeStationsTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BikeStationsTableViewController: UITableViewController{
     
+    private var stationListVM: BikeStationListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -18,9 +20,37 @@ class BikeStationsTableViewController: UITableViewController{
         self.navigationController?.navigationBar.prefersLargeTitles = true
         let url = URL(string: "http://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=stacje_rowerowe")!
         
-        Webservice().getBikeStations(url: url) { _ in
+        Webservice().getBikeStations(url: url) { station in
             
-        }
+            if let station = station{
+                self.stationListVM = BikeStationListViewModel(Station: station)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         
     }
+}
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.stationListVM == nil ? 0 : self.stationListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.stationListVM.numberOfRowsInSection(section)
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "StationTableViewCell", for: indexPath) as? StationTableViewCell else {
+            fatalError("Cell not found")
+            
+        }
+     let stationVM =   self.stationListVM.stationAtIndex(indexPath.row)
+        cell.bike_racksLabel.text = stationVM.bike_racks
+        cell.bikesLabel.text = stationVM.bikes
+        cell.labelLabel.text = stationVM.label
+        return cell
+    }
+    
 }
